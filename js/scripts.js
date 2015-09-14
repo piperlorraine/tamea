a533a7fe1731283b2cb1f72f72f5f50cf3873b31
Piper Semerau <piperlorraine@gmail.com>
2015-09-13 23:26:49 -0700

    finishing up the basic page stylings

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,36 @@ $(document).ready(function(){
 		$(this).find("ul").fadeIn();
 	}).mouseleave(function() {
 		$(this).find("ul").fadeOut();
-	}).click(function() {
-		$(this).find("ul").toggle();
 	});
+        
+        var width, height;
+        if($(".youtube").length) {
+            if($(".video-box").width() > 420) {
+               width = 420;
+               height = 315;
+           } else {
+               width = $(".video-box").width();
+               height = (315/420) * width;
+           }
+        }
+        $(".youtube").each(function() {
+           var id = $(this).attr("data-video");
+           var content = '<iframe width="'+width+'" height="'+height+'" src="https://www.youtube.com/embed/'+id+'" frameborder="0" allowfullscreen></iframe>';
+           $(this).html(content);
+        });
+            
 
+        
+    $(function() {
+      $('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        play: {
+          active: true,
+          auto: true,
+          interval: 4000,
+          swap: true
+        }
+      });
+    });
 });
